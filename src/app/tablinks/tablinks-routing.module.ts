f77de3bec53ad7cce8bcfbbc274369eacdf441e5
Alexandre DAVAULT <alexandre.davault@ynov.com>
2022-01-26 17:44:04 +0100

    comments deleted

--- src/app/tablinks/tablinks-routing.module.ts
+++ src/app/tablinks/tablinks-routing.module.ts
@@ -3,37 +3,6 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { TablinksPage } from './tablinks.page';
 
-// const routes: Routes = [
-//   {
-//     path: 'tablinks',
-//     component: TablinksPage,
-//     children: [
-//       {
-//         path: 'home',
-//         loadChildren: () => import('../home/home.module').then(m => m.HomePageModule)
-//       },
-//       {
-//         path: 'login',
-//         loadChildren: () => import('../login/login.module').then(m => m.LoginPageModule)
-//       },
-//       {
-//         path: 'profile',
-//         loadChildren: () => import('../profil/profil.module').then(m => m.ProfilPageModule)
-//       },
-//       {
-//         path: '',
-//         redirectTo: '/tablinks/home',
-//         pathMatch: 'full'
-//       }
-//     ]
-//   },
-//   {
-//     path: '',
-//     redirectTo: '/tablinks/home',
-//     pathMatch: 'full'
-//   }
-// ];
-
 const routes: Routes = [  
   {
    path: '',    
@@ -64,18 +33,6 @@ const routes: Routes = [
     ]
   }
 ]
-
-
-
-    // { 
-    //   path: 'dashboard',
-    //   loadChildren: './dashboard/dashboard.module#DashboardModule' 
-    // },
-    // { 
-    //   path: 'drivers',
-    //   loadChildren: './drivers/drivers.module#DriversModule' 
-    // },
-  //  ]
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
